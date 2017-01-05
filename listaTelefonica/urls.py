9c696f6304c94ff8183003a4efb6557c11bc6305
Júnior Carvalho <joseadolfojr@gmail.com>
2017-01-05 21:06:44 -0200

    admin

--- listaTelefonica/urls.py
+++ listaTelefonica/urls.py
@@ -4,7 +4,7 @@ from listaTelefonica.core.views import contato_api, operadora_api
 
 urlpatterns = [
 
-    url(r'^admin/', admin.site.urls),
+    url(r'^', admin.site.urls),
     url(r'^contato/$', contato_api),
     url(r'^operadora/$', operadora_api),
 ]
